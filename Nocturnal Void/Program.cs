8c1619fc2200750e2df150f0b6261cf35841584a
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-07-22 13:59:57 -0300

    Import new VS projects
    I messed up the old ones.

---
+++ Nocturnal Void/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Nocturnal_Void
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
