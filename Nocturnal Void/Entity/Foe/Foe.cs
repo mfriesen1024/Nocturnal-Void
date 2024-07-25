b760c2fbfe2d8fe38ca30dcaa30ad8e437de601a
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-07-25 16:01:25 -0300

    We probably need polymorphism here.
    I plan to split mob into player and foe. I think it makes sense for players and foes to have the same update system.

--- Nocturnal Void/Entity/Foe/Foe.cs
+++ Nocturnal Void/Entity/Foe/Foe.cs
@@ -6,17 +6,8 @@ namespace Nocturnal_Void.Entity.Foe
     /// <summary>
     /// Represents an opponent.
     /// </summary>
-    internal class Foe
+    internal class Foe : Mob
     {
-        public string name;
-
-        public StatManager statMan { get; protected set; }
-
-        public int def { get; protected set; }
-        public int str { get; protected set; }
-
-        public RelativeRenderable renderable { get; protected set; }
-
-        public Hitbox hitbox { get; protected set; }
+        
     }
 }
---
+++ Nocturnal Void/Entity/Mob.cs
@@ -0,0 +1,27 @@
+﻿using Nocturnal_Void.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TZPRenderers.Text;
+
+namespace Nocturnal_Void.Entity
+{
+    /// <summary>
+    /// Represents a mob(ile) entity.
+    /// </summary>
+    internal abstract class Mob
+    {
+        public string name;
+
+        public StatManager statMan { get; protected set; }
+
+        public int def { get; protected set; }
+        public int str { get; protected set; }
+
+        public RelativeRenderable renderable { get; protected set; }
+
+        public Hitbox hitbox { get; protected set; }
+    }
+}
