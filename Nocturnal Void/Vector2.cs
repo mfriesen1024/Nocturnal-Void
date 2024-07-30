4d3ae18e621299f377ea6144f0c521c9e93da884
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-07-30 15:53:33 -0300

    Add vector2

---
+++ Nocturnal Void/Vector2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nocturnal_Void
+{
+    internal struct Vector2
+    {
+        public int x, y;
+
+        public static explicit operator Vector2(byte[] bytes)
+        {
+            if (bytes.Length != 8) throw new ArgumentException($"Byte array must be of length 8. It was {bytes.Length}");
+
+            int x = BitConverter.ToInt32(bytes, 0);
+            int y = BitConverter.ToInt32(bytes, 4);
+
+            return new Vector2() { x = x, y = y };
+        }
+    }
+}
