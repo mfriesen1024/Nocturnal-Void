6b2ba93d348b12a7383b29e06636278888549cc4
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-01 15:44:16 -0300

    Create Gold.cs

---
+++ Nocturnal Void/Entity/Items/Gold.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nocturnal_Void.Entity.Items
+{
+    internal class Gold : Item
+    {
+        public int value = 1;
+        public override Item Clone()
+        {
+            return (Item)MemberwiseClone();
+        }
+    }
+}
