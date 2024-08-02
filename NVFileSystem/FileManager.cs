be142587484b1a41387d88113e5e580c7816818d
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-02 17:44:08 -0300

    Start loader method

--- NVFileSystem/FileManager.cs
+++ NVFileSystem/FileManager.cs
@@ -1,7 +1,24 @@
-﻿namespace NVFileSystem
-{
-    public class FileManager
-    {
+﻿using NVFileSystem.Loaders;
+using NVFileSystem.Util;
+using File = NVFileSystem.Util.File;
 
+namespace NVFileSystem
+{
+    public static class FileManager
+    {
+        public static ItemLoader ItemLoader { get; private set; } = new ItemLoader();
+
+
+        public static void Load(string path)
+        {
+            File file = new File(path);
+
+            ItemLoader.Load(file);
+        }
+
+        public static void Save(string path)
+        {
+
+        }        
     }
 }
