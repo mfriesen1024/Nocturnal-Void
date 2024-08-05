1ebfb5c0fa57d82851a7d74daf1a8ed91f29698d
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-05 16:06:32 -0300

    Update FileManager.cs

--- Nocturnal Void/FileSystem/FileManager.cs
+++ Nocturnal Void/FileSystem/FileManager.cs
@@ -6,18 +6,22 @@ namespace Nocturnal_Void.FileSystem
     public static class FileManager
     {
         public static ItemLoader ItemLoader { get; private set; } = new ItemLoader("items.bin");
-
+        public static EntityLoader EntityLoader { get; private set; } = new EntityLoader("entities.bin");
 
         public static void Load(string path)
         {
-            File file = new File(path);
+            File dir = new File(path);
 
-            ItemLoader.Load(file);
+            ItemLoader.Load(dir);
+            EntityLoader.Load(dir);
         }
 
         public static void Save(string path)
         {
+            File dir = new File(path);
 
+            ItemLoader.Save(dir);
+            EntityLoader.Save(dir);
         }
     }
 }
