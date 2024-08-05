6112764407feaf87f6513b2604542db3686f6c08
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-05 16:32:50 -0300

    Create unit tests.

---
+++ Nocturnal VoidTests/FileSystem/FileManagerFilesystemTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nocturnal_Void.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nocturnal_Void.FileSystem.Tests
+{
+    [TestClass()]
+    public class FileManagerTests
+    {
+        [TestMethod()]
+        public void LoadTest()
+        {
+            FileManager.Load("TestFolder");
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            FileManager.Save("TestFolder");
+        }
+    }
+}
