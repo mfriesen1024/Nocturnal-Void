ac0f034ab6eff30cda466500e969fdf6ed742de5
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-06 11:24:52 -0300

    Setup invocation system.

--- NVCampaignEditor/CommandBase.cs
+++ NVCampaignEditor/CommandBase.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NVCampaignEditor
+﻿namespace NVCampaignEditor
 {
-    internal class CommandBase
+    abstract class CommandBase
     {
+        private string[] aliases;
+        private CommandBase[] subcommands = [];
+
+        public string[] Aliases { get => aliases; protected set => aliases = value; }
+        public CommandBase[] Subcommands { get => subcommands; protected set => subcommands = value; }
+
+        /// <summary>
+        /// Used to execute the command base, first checking subcommands, then processing the command.
+        /// </summary>
+        /// <param name="argArray">The arguments to be used.</param>
+        public virtual void Invoke(string[] argArray)
+        {
+            var argList = argArray.ToList();
+            string s1 = argList[0]; // Check this against subcommand aliases.
+
+            foreach (CommandBase subcommand in subcommands)
+            {
+                if (subcommand.Aliases != null)
+                {
+                    if (subcommand.Aliases.Contains(s1))
+                    {
+                        // In theory, this shouldnt cause issues, but be prepared to define a new var for this.
+                        // Remove the subcommand from the args we pass.
+                        argList.RemoveAt(0);
+                        subcommand.Invoke(argList.ToArray()); return;
+                    }
+                }
+            }
+
+            Process(argArray);
+        }
+
+        /// <summary>
+        /// Called if no subcommands were invoked by Invoke().
+        /// This should process the command, and be prepared to handle invalid arguments.
+        /// </summary>
+        /// <param name="argArray">The arguments to be used.</param>
+        protected abstract void Process(string[] argArray);
     }
 }
