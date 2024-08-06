a51528a80766589eab6a74803451ba4adffa7806
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-06 12:09:58 -0300

    Create EntryCommand.cs

---
+++ NVCampaignEditor/Command/EntryCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVCampaignEditor.Command
+{
+    internal class EntryCommand : CommandBase
+    {
+        public EntryCommand()
+        {
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            throw new ArgumentException("Invalid command.");
+        }
+    }
+}
