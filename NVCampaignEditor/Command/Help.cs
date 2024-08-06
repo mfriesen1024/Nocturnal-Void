e85259a5e050ba81b993dd97f343255e92229565
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-06 12:17:59 -0300

    Create Help.cs

---
+++ NVCampaignEditor/Command/Help.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVCampaignEditor.Command
+{
+    internal class Help : CommandBase
+    {
+        public Help()
+        {
+            Aliases = ["help"];
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            string helpMsg = "";
+
+            Console.WriteLine(helpMsg);
+        }
+    }
+}
