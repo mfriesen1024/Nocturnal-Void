742f3c77fe41b1f23e6d9de6a9f911192717e400
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-06 12:07:46 -0300

    Create entry point.

--- NVCampaignEditor/Program.cs
+++ NVCampaignEditor/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace NVCampaignEditor
+﻿using NVCampaignEditor.Util;
+using System.Reflection;
+
+namespace NVCampaignEditor
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // EntryCommand entry = new EntryCommand();
+            Startup();
+            Loop();
+
+            void Startup()
+            {
+                Assembly assembly = Assembly.GetEntryAssembly();
+                if (assembly == null) { Environment.Exit(1); return; }
+
+                AssemblyName name = assembly.GetName();
+                Version v = name.Version;
+
+
+                Console.WriteLine(name.Name + " Version " + v);
+            }
+
+            void Loop()
+            {
+                while (true)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Enter a command, or \"help\" for help");
+                    Console.WriteLine();
+
+                    string[] args = CommandParser.ParseArgs(Console.ReadLine());
+                    //entry.Invoke(args);
+                }
+            }
         }
     }
 }
