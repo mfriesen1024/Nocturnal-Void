bd934cd0138e2759d9ecb86703c095113c1a94e4
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-07 15:58:31 -0300

    Update FileManager.cs

--- Nocturnal Void/FileSystem/FileManager.cs
+++ Nocturnal Void/FileSystem/FileManager.cs
@@ -50,7 +50,7 @@ namespace Nocturnal_Void.FileSystem
 
             // Entity init.
             EntityLoader.SetPlayer(new Player("Player", 100, 0,10,Vector2.Zero, renderable));
-            Foe[] foes = [new Foe("SampleFoe", 10, 5, 2, Vector2.Zero, renderable)];
+            Foe[] foes = [new Foe("SampleFoe", 10, 5, 2, 2, Vector2.Zero, renderable)];
             EntityLoader.SetFoes(foes);
         }
     }
