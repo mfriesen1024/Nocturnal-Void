72ad3f04b16928f009a663fc50927abf6afa9cbf
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-07 16:46:13 -0300

    Update Help.cs

--- NVCampaignEditor/Command/Help.cs
+++ NVCampaignEditor/Command/Help.cs
@@ -15,7 +15,17 @@ namespace NVCampaignEditor.Command
 
         protected override void Process(string[] argArray)
         {
-            string helpMsg = "";
+            string helpMsg = "" +
+                "Load: Loads all data from the path specified\n" +
+                "Usage: load <path>\n\n" +
+                "Save: Saves all data to the path specified\n" +
+                "Usage: save <path>\n\n" +
+                "Init: Generates new data so things dont break when loads/saves/updates are called.\n" +
+                "Usage: init\n\n" +
+                "Exit: Exits the editor.\n" +
+                "Usage: exit\n\n" +
+                "Help: Displays this message.\n" +
+                "Usage: help\n\n";
 
             Console.WriteLine(helpMsg);
         }
