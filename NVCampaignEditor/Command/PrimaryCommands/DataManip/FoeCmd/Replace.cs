86e4940d3cf3623910aa0b8747e4c7bbb444387f
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-07 19:49:33 -0300

    Namespace adjustment

--- NVCampaignEditor/Command/PrimaryCommands/DataManip/FoeCmd/Replace.cs
+++ NVCampaignEditor/Command/PrimaryCommands/DataManip/FoeCmd/Replace.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.Player
+namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.FoeCmd
 {
     internal class Replace : CommandBase
     {
