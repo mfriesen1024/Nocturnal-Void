1817c1b9f51da51c9216429a6f5621f3ac182908
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-08 15:21:16 -0300

    Create consumable commands

---
+++ NVCampaignEditor/Command/PrimaryCommands/DataManip/CItem/CConsumable/ConsumableBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.C;
+using NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CGold;
+
+namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CConsumable
+{
+    internal class ConsumableBase : CommandBase
+    {
+        public ConsumableBase()
+        {
+            Aliases = ["consumable", "c"];
+            Subcommands = [new Replace(), new Create(), new Delete()];
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            throw new ArgumentException("Invalid command.");
+        }
+    }
+}
---
+++ NVCampaignEditor/Command/PrimaryCommands/DataManip/CItem/CConsumable/Create.cs
@@ -0,0 +1,37 @@
+﻿using Nocturnal_Void.Entity.Items;
+using Nocturnal_Void.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CConsumable
+{
+    internal class Create : CommandBase
+    {
+        public Create()
+        {
+            Aliases = ["create", "c"];
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            var objs = FileManager.ItemLoader.Consumables.ToList();
+
+            if(argArray.Length < 2) { objs.Add(AskUser()); }
+            else { objs.Add(new Consumable((Consumable.ConsumableType)int.Parse(argArray[0]), int.Parse(argArray[1]))); }
+            
+            FileManager.ItemLoader.SetConsumables(objs.ToArray());
+        }
+
+        public static Consumable AskUser()
+        {
+            Console.WriteLine($"Currently only heal type is supported. Please enter \"0\"");
+            int typeInt = int.Parse( Console.ReadLine() );
+            Console.WriteLine($"Enter the value for the item.");
+            int value = int.Parse( Console.ReadLine() );
+            return new Consumable((Consumable.ConsumableType)typeInt, value);
+        }
+    }
+}
