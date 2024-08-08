e9f34b92996b08c5f310cde65342c13cc8d4ea0c
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-08 15:34:20 -0300

    Create ItemBase.cs

---
+++ NVCampaignEditor/Command/PrimaryCommands/DataManip/CItem/ItemBase.cs
@@ -0,0 +1,25 @@
+﻿using NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CConsumable;
+using NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CEquip;
+using NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CGold;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem
+{
+    internal class ItemBase : CommandBase
+    {
+        public ItemBase()
+        {
+            Aliases = ["item", "items", "i"];
+            Subcommands = [new ConsumableBase(), new EquipmentBase(), new GoldBase()];
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
