4e7a729dba9f7467b1bfcfb8eab62cea62babaf4
Zyganadel <144160470+mfriesen1024@users.noreply.github.com>
2024-08-08 15:29:54 -0300

    Add equip commands

---
+++ NVCampaignEditor/Command/PrimaryCommands/DataManip/CItem/CEquip/Delete.cs
@@ -0,0 +1,19 @@
+﻿using Nocturnal_Void.FileSystem;
+
+namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CEquip
+{
+    internal class Delete : CommandBase
+    {
+        public Delete()
+        {
+            Aliases = ["delete", "del", "d"];
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            var objs = FileManager.ItemLoader.Equip.ToList();
+            objs.RemoveAt(int.Parse(argArray[0]));
+            FileManager.ItemLoader.SetEquip(objs.ToArray());
+        }
+    }
+}
---
+++ NVCampaignEditor/Command/PrimaryCommands/DataManip/CItem/CEquip/EquipmentBase.cs
@@ -0,0 +1,16 @@
+﻿namespace NVCampaignEditor.Command.PrimaryCommands.DataManip.CItem.CEquip
+{
+    internal class EquipmentBase : CommandBase
+    {
+        public EquipmentBase()
+        {
+            Aliases = ["equipment", "equip", "e"];
+            Subcommands = [new Replace(), new Create(), new Delete()];
+        }
+
+        protected override void Process(string[] argArray)
+        {
+            throw new ArgumentException("Invalid command.");
+        }
+    }
+}
